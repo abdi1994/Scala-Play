2170080d9987e717081845eb4bfff200a290d130
abdi1994 <abdi1994@hotmail.com>
2017-07-24 17:11:19 +0100

    Second Commit

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,6 +1,8 @@
 package controllers
 
+import akka.util.ByteString
 import play.api._
+import play.api.http.HttpEntity
 import play.api.mvc._
 
 class Application extends Controller {
@@ -12,5 +14,28 @@ class Application extends Controller {
   def secondPage = Action {
     Ok(views.html.secondPage())
   }
+
+  def thirdPage(name: String) = Action {
+    Ok("Hello " + name)
+  }
+
+  def fourthPage = Action {
+    Result(
+      header = ResponseHeader(1000000, Map.empty),
+      body = HttpEntity.Strict(ByteString("Hello world"), Some("text/plain"))
+    )
+  }
+
+  def fifthPage = Action {
+    Redirect("/third?name=Bob", MOVED_PERMANENTLY)
+  }
+
+  def sixthPage = TODO
+
+  def seventhPage(number: Int) = Action {
+    Ok("Square of " + number + " is " + number*number)
+  }
+
+
 
 }
