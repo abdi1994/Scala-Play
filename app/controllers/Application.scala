a2217bc485193065efd55078518a7355028d8819
abdi1994 <abdi1994@hotmail.com>
2017-07-26 14:17:17 +0100

    Form exercises

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,8 +4,15 @@ import akka.util.ByteString
 import play.api._
 import play.api.http.HttpEntity
 import play.api.mvc._
+import javax.inject.Inject
 
-class Application extends Controller {
+import models.{Item, delItem}
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc._
+
+import scala.concurrent.Future
+
+class Application @Inject()(val messagesApi: MessagesApi) extends Controller with I18nSupport {
 
   def index : Action[AnyContent] = Action {
     Ok(views.html.index("Your new application is ready.")).withSession("connected" -> "user@gmail.com")
@@ -40,6 +47,40 @@ class Application extends Controller {
   def seventhPage(number: Int) : Action[AnyContent] = Action {
     Ok("Square of " + number + " is " + number*number)
   }
+
+
+  def listItems = Action { implicit request =>
+    Ok(views.html.Items(Item.items, Item.createItemForm, delItem.deleteItemForm))
+  }
+
+  def createItem = Action { implicit request =>
+
+    val formValidationResult = Item.createItemForm.bindFromRequest
+    formValidationResult.fold({ formWithErrors =>
+      BadRequest(views.html.Items(Item.items, formWithErrors, delItem.deleteItemForm))
+    }, { item =>
+      Item.items.append(item)
+      Redirect(routes.Application.listItems())
+    })
+  }
+
+  def deleteItem = Action { implicit  request =>
+    val formValidationResult = delItem.deleteItemForm.bindFromRequest
+    formValidationResult.fold({ formWithErrors =>
+      BadRequest(views.html.Items(Item.items, Item.createItemForm, formWithErrors))
+    }, { item =>
+      Item.items.foreach(x => if(x.name.equals(item.name)) {Item.items -= x})
+      Redirect(routes.Application.listItems())
+    })
+  }
+
+
+  def updateItem = Action { implicit request =>
+
+    Ok(views.html.Items(Item.items, Item.createItemForm.fill(Item.items.head), delItem.deleteItemForm))
+
+  }
+
 
 
 
